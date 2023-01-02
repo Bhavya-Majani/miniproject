f932055f5cebbc07bbc4162eac5874d42a661131
Bhavya-Majani <117262756+Bhavya-Majani@users.noreply.github.com>
2023-01-02 17:39:51 +0530

    Add files via upload

---
+++ Miniproject_javadbms/src/miniproject_javadbms/Miniproject_javadbms.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniproject_javadbms;
+
+/**
+ *
+ * @author Adit Kanaji
+ */
+public class Miniproject_javadbms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
